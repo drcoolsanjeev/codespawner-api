b1d8eff28f9d454e2320482078ea8264c9f14359
iamsaquib <saquibulhassan6@gmail.com>
2020-01-20 01:18:29 +0530

    initial  comit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CodeSpawner
+An Online Judge Platform API for Coding Contests Using Golang
+
+Work in Progress, rewriting the whole API again
---
+++ gql/types.go
@@ -0,0 +1,24 @@
+package gql
+
+import "github.com/graphql-go/graphql"
+
+// User describes a graphql object containing a User
+var User = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"email": &graphql.Field{
+				Type: graphql.String,
+			},
+			"type": &graphql.Field{
+				Type: graphql.Int,
+			},
+		},
+	},
+)
